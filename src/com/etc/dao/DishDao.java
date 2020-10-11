a76aeb703493636be0c34414acea7780bfb532a9
CallMeSiJue <1105553966@qq.com>
2020-10-11 17:21:33 +0800

    Dish

---
+++ src/com/etc/dao/DishDao.java
@@ -0,0 +1,27 @@
+package com.etc.dao;
+
+import com.etc.entity.Customer;
+import com.etc.entity.Disher;
+import com.etc.util.DBUtils;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DishDao {
+    public List<Disher> query() throws SQLException {
+
+        ArrayList<Disher> list=new ArrayList<Disher>();
+
+        ResultSet rs = DBUtils.doQuery("select * from disher ");
+        while(rs.next()){
+
+            list.add(new Disher(rs.getInt("did"),rs.getString("dishname"),
+                    rs.getInt("number"),rs.getString("rmaterial"),
+                    rs.getInt("price"),rs.getInt("sid")));
+
+        }
+        return list;
+    }
+}
