3e7277c00ac15878395d1c6a1a9b61af287352c4
king <1214965382@qq.com>
2020-10-12 18:34:41 +0800

    sb

--- src/main/java/com/etc/util/DBUtils.java
+++ src/main/java/com/etc/util/DBUtils.java
@@ -17,7 +17,7 @@ public class DBUtils {
 	private static final String DRIVERNAME = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://127.0.0.1:3306/test";
 	private static final String USERNAME = "root";
-	private static final String PASSWORD = "root";
+	private static final String PASSWORD = "123456";
 
 
 	public static Connection getConn() {
